f34cae5aed9fa1bf88dedd810e4b57b5ee653196
Sisipho Mdaka <218319762@mycput.ac.za>
2025-05-13 15:47:34 +0200

    test

--- tests/test_inmemory_student_repository.py
+++ tests/test_inmemory_student_repository.py
@@ -1,5 +1,4 @@
-import pytest
-from repositories.inmemory.inmemory_student_repository import InMemoryStudentRepository
+from src.repositories.inmemory.inmemory_student_repository import InMemoryStudentRepository # type: ignore
 from domain.student import Student
 
 def test_save_and_find():
